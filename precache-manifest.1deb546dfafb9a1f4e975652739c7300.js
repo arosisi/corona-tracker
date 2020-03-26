67f4f13277764d785cd696c8ef757c54702c3d48
Tom Nguyen <tom.trinh.nguyen@gmail.com>
2020-03-25 23:26:49 -0400

    Updates

---
+++ precache-manifest.1deb546dfafb9a1f4e975652739c7300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4752a4033161ba754cd37410e35087a",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "c2354bcc5a6290de1902",
+    "url": "/corona-tracker/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d45ef25a662a5c63f4dc",
+    "url": "/corona-tracker/static/js/2.70b7dfa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-tracker/static/js/2.70b7dfa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2354bcc5a6290de1902",
+    "url": "/corona-tracker/static/js/main.0685022c.chunk.js"
+  },
+  {
+    "revision": "461798bcea028e2f4893",
+    "url": "/corona-tracker/static/js/runtime-main.db302e28.js"
+  }
+]);
